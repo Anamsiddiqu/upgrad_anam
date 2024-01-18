6a618fab217b3990f583d31dc966c828897023fb
Anamsiddiqu <anamsiddiqui5600@gmail.com>
2024-01-18 22:07:49 +0530

    [ Adding two Numbers]

---
+++ AddNumbers.java
@@ -0,0 +1,16 @@
+class AddNumbers {
+    public static void main(String[] args) {
+        // Declaring variable
+        float num1 = 5;
+        float num2 = 10;
+        // Adding Numbers
+        float sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        // Substracting Numbers
+        float sub = num1 - num2 ;
+        System.out.println("sub: " + sub);
+        // Dividing Numbers
+        float Div = num1 / num2 ;
+        System.out.println("Div: " + Div);
+    }
+}
