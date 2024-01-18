108968d239e4f4fce1e5b0f9868ed36e82fdd8af
Anamsiddiqu <anamsiddiqui5600@gmail.com>
2024-01-18 22:30:15 +0530

    [Even Or Odd Example in Java]

---
+++ PrintNumbers.java
@@ -0,0 +1,8 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        // For Loop Added
+        for (int i = 1; i <= 27; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
