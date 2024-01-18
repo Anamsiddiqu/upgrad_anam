655f1eb4470c26d5a711c23e68eea6e4d479af3e
Anamsiddiqu <anamsiddiqui5600@gmail.com>
2024-01-18 19:54:40 +0530

    [String concatenation example in java]

---
+++ StringConcatenationExample.java
@@ -0,0 +1,12 @@
+public class StringConcatenationExample {
+    public static void main(String[] args) {
+        String firstName = "siddiqui";
+        String lastName = "anam";
+
+        // String concatenation
+        String fullName = firstName + " " + lastName;
+
+        // Output
+        System.out.println("Full Name: " + fullName);
+    }
+}
