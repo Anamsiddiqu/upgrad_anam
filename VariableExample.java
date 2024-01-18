6363618df093c097eb2f686119d8aa191e69f131
Anamsiddiqu <anamsiddiqui5600@gmail.com>
2024-01-18 20:23:22 +0530

    [variable example in java]

---
+++ VariableExample.java
@@ -0,0 +1,17 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        // Declaration and initialization of variables
+        int age = 22;            // Integer
+        double height = 5.3;     // Double
+        char gender = 'F';       // Character
+        boolean isStudent = true; // Boolean
+        String name = "anam";    // String
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Gender: " + gender);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
